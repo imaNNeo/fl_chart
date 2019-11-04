c70141ed325f573d1061fc78da0e3f84f0851415
imaNNeoFighT <iman.neofight@gmail.com>
2019-11-05 01:41:50 +0330

    updated line_chart_sample5

--- example/lib/line_chart/samples/line_chart_sample5.dart
+++ example/lib/line_chart/samples/line_chart_sample5.dart
@@ -2,33 +2,60 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class LineChartSample5 extends StatelessWidget {
+  final List<int> showIndexes = const [1, 3, 5];
+
   @override
   Widget build(BuildContext context) {
+    final lineBarsData = [
+      LineChartBarData(
+        showingIndicators: showIndexes,
+        spots: [
+          FlSpot(0, 1),
+          FlSpot(1, 2),
+          FlSpot(2, 1.5),
+          FlSpot(3, 3),
+          FlSpot(4, 3.5),
+          FlSpot(5, 5),
+          FlSpot(6, 8),
+        ],
+        isCurved: true,
+        barWidth: 4,
+        belowBarData: BarAreaData(
+          show: true,
+        ),
+        dotData: FlDotData(show: false),
+      ),
+    ];
+
+    final LineChartBarData tooltipsOnBar = lineBarsData[0];
+
     return SizedBox(
       width: 300,
       height: 140,
       child: LineChart(
         LineChartData(
-          lineTouchData: const LineTouchData(enabled: false),
-          lineBarsData: [
-            const LineChartBarData(
-              spots: [
-                FlSpot(0, 1),
-                FlSpot(1, 2),
-                FlSpot(2, 1.5),
-                FlSpot(3, 3),
-                FlSpot(4, 3.5),
-                FlSpot(5, 5),
-                FlSpot(6, 8),
+          showingTooltipIndicators: showIndexes.map((index) {
+            return MapEntry(
+              index,
+              [
+                LineBarSpot(tooltipsOnBar, lineBarsData.indexOf(tooltipsOnBar), tooltipsOnBar.spots[index]),
               ],
-              isCurved: true,
-              barWidth: 4,
-              belowBarData: BarAreaData(
-                show: true,
-              ),
-              dotData: FlDotData(show: false),
-            ),
-          ],
+            );
+          }).toList(),
+          lineTouchData: LineTouchData(
+              enabled: false,
+              touchTooltipData: LineTouchTooltipData(
+                  tooltipBgColor: Colors.pink,
+                  tooltipRoundedRadius: 8,
+                  getTooltipItems: (List<LineBarSpot> lineBarsSpot) {
+                    return lineBarsSpot.map((lineBarSpot) {
+                      return LineTooltipItem(
+                        lineBarSpot.y.toString(),
+                        TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      );
+                    }).toList();
+                  })),
+          lineBarsData: lineBarsData,
           minY: 0,
           titlesData: FlTitlesData(
             leftTitles: const SideTitles(
