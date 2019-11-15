42f36d26c650f71c8f7a8f9d552981c6b8b867b8
imaNNeoFighT <iman.neofight@gmail.com>
2019-11-15 21:12:02 +0330

    added bar_chart_sample4

--- example/lib/line_chart/line_chart_page4.dart
+++ example/lib/line_chart/line_chart_page4.dart
@@ -1,15 +1,18 @@
 import 'package:example/bar_chart/samples/bar_chart_sample3.dart';
+import 'package:example/bar_chart/samples/bar_chart_sample4.dart';
 import 'package:flutter/material.dart';
 
 class LineChartPage4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color(0xfff3f3f5),
       body: ListView(
         padding: const EdgeInsets.all(24),
         children: <Widget>[
           BarChartSample3(),
+          const SizedBox(height: 18,),
+          BarChartSample4(),
         ],
       ),
     );
