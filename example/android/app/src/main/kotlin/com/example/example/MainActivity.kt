6f9ba213e394553b9c24d9c038551de627a07d1f
Enzo Lizama <lizama.enzo@gmail.com>
2020-01-04 16:59:32 -0500

    migrate fl_chart example to AndroidX

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.example
 
-import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
 }
