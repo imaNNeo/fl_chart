e92b093ca27587d0d50618c9a8b44c623d7cebf2
Timothy Shamilov <tim@mothic.com>
2020-01-20 13:57:19 -0500

    docs(dashed-lines): comment typo

--- lib/src/extensions/path_extension.dart
+++ lib/src/extensions/path_extension.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:path_drawing/path_drawing.dart';
 
 extension DashedPath on Path {
-  /// get a dashed(or solid) path from two a path
+  /// get a dashed(or solid) path from a path
   Path toDashedPath(List<int> dashArray) {
     if (this != null && dashArray != null) {
       final castedArray = dashArray.map((value) => value.toDouble()).toList();
