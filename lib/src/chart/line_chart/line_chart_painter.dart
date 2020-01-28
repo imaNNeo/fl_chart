2269d2aba91dd24c4dcc26f37e26a5fced01c975
Ruslan Gataullin <fisher7410@yandex.ru>
2020-01-28 19:16:46 +0500

    Implemented line label painter

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -1,4 +1,5 @@
 import 'dart:ui' as ui;
+import 'dart:ui';
 
 import 'package:fl_chart/fl_chart.dart';
 import 'package:fl_chart/src/chart/base/axis_chart/axis_chart_data.dart';
@@ -796,12 +797,41 @@ class LineChartPainter extends AxisChartPainter<LineChartData>
 
         final double rightChartPadding = getExtraNeededHorizontalSpace() - getLeftOffsetDrawSize();
         final Offset to =
-        Offset(viewSize.width - rightChartPadding, getPixelY(line.y, chartUsableSize));
+            Offset(viewSize.width - rightChartPadding, getPixelY(line.y, chartUsableSize));
 
         extraLinesPaint.color = line.color;
         extraLinesPaint.strokeWidth = line.strokeWidth;
 
         canvas.drawDashedLine(from, to, extraLinesPaint, line.dashArray);
+
+        if (line.label != null) {
+          final HorizontalLineLabel label = line.label;
+          final TextStyle style = TextStyle(fontSize: 11, color: line.color).merge(label.style);
+          final EdgeInsets padding = label.padding ?? EdgeInsets.zero;
+
+          final TextSpan span = TextSpan(
+            text: label.labelResolver(line),
+            style: style,
+          );
+
+          final TextPainter tp = TextPainter(
+            text: span,
+            textDirection: TextDirection.ltr,
+          );
+
+          tp.layout();
+          tp.paint(
+            canvas,
+            label.alignment.withinRect(
+              Rect.fromLTRB(
+                from.dx + padding.left,
+                from.dy - padding.bottom - tp.height,
+                to.dx - padding.right - tp.width,
+                to.dy + padding.top,
+              ),
+            ),
+          );
+        }
       }
     }
 
@@ -812,12 +842,42 @@ class LineChartPainter extends AxisChartPainter<LineChartData>
 
         final double bottomChartPadding = getExtraNeededVerticalSpace() - getTopOffsetDrawSize();
         final Offset to =
-        Offset(getPixelX(line.x, chartUsableSize), viewSize.height - bottomChartPadding);
+            Offset(getPixelX(line.x, chartUsableSize), viewSize.height - bottomChartPadding);
 
         extraLinesPaint.color = line.color;
         extraLinesPaint.strokeWidth = line.strokeWidth;
 
         canvas.drawDashedLine(from, to, extraLinesPaint, line.dashArray);
+
+        if (line.label != null) {
+          final VerticalLineLabel label = line.label;
+          final TextStyle style = TextStyle(fontSize: 11, color: line.color).merge(label.style);
+          final EdgeInsets padding = label.padding ?? EdgeInsets.zero;
+
+          final TextSpan span = TextSpan(
+            text: label.labelResolver(line),
+            style: style,
+          );
+
+          final TextPainter tp = TextPainter(
+            text: span,
+            textDirection: TextDirection.ltr,
+          );
+
+          tp.layout();
+
+          tp.paint(
+            canvas,
+            label.alignment.withinRect(
+              Rect.fromLTRB(
+                to.dx - padding.right - tp.width,
+                from.dy + padding.top - topChartPadding,
+                from.dx + padding.left,
+                to.dy - padding.bottom + bottomChartPadding,
+              ),
+            ),
+          );
+        }
       }
     }
   }
