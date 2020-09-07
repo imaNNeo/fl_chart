9b16aaa2daaa55d0e173855c212daf7d632a01b9
Giraldi Maggio <giraldi.maggio@yahoo.com>
2020-09-07 10:20:11 +0700

    Updated sample file with badgeWidgets set for each sections

--- example/lib/pie_chart/samples/pie_chart_sample3.dart
+++ example/lib/pie_chart/samples/pie_chart_sample3.dart
@@ -1,5 +1,7 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:fl_chart/fl_chart.dart';
 
 class PieChartSample3 extends StatefulWidget {
   @override
@@ -44,7 +46,7 @@ class PieChartSample3State extends State {
   List<PieChartSectionData> showingSections() {
     return List.generate(4, (i) {
       final isTouched = i == touchedIndex;
-      final double fontSize = isTouched ? 25 : 16;
+      final double fontSize = isTouched ? 20 : 16;
       final double radius = isTouched ? 110 : 100;
       final double widgetSize = isTouched ? 55 : 40;
 
@@ -54,19 +56,15 @@ class PieChartSample3State extends State {
             color: const Color(0xff0293ee),
             value: 40,
             title: '40%',
-            badgeWidget: AnimatedContainer(
-              duration: PieChart.defaultDuration,
-              width: widgetSize,
-              height: widgetSize,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                shape: BoxShape.circle,
-              ),
-            ),
-            badgePositionPercentageOffset: .98,
             radius: radius,
             titleStyle: TextStyle(
                 fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+            badgeWidget: _Badge(
+              'assets/ophthalmology-svgrepo-com.svg',
+              size: widgetSize,
+              borderColor: const Color(0xff0293ee),
+            ),
+            badgePositionPercentageOffset: .98,
           );
         case 1:
           return PieChartSectionData(
@@ -76,25 +74,27 @@ class PieChartSample3State extends State {
             radius: radius,
             titleStyle: TextStyle(
                 fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+            badgeWidget: _Badge(
+              'assets/librarian-svgrepo-com.svg',
+              size: widgetSize,
+              borderColor: const Color(0xfff8b250),
+            ),
+            badgePositionPercentageOffset: .98,
           );
         case 2:
           return PieChartSectionData(
             color: const Color(0xff845bef),
             value: 16,
             title: '16%',
-            badgeWidget: AnimatedContainer(
-              duration: PieChart.defaultDuration,
-              width: widgetSize,
-              height: widgetSize,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                shape: BoxShape.circle,
-              ),
-            ),
-            badgePositionPercentageOffset: .98,
             radius: radius,
             titleStyle: TextStyle(
                 fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+            badgeWidget: _Badge(
+              'assets/fitness-svgrepo-com.svg',
+              size: widgetSize,
+              borderColor: const Color(0xff845bef),
+            ),
+            badgePositionPercentageOffset: .98,
           );
         case 3:
           return PieChartSectionData(
@@ -104,10 +104,60 @@ class PieChartSample3State extends State {
             radius: radius,
             titleStyle: TextStyle(
                 fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
+            badgeWidget: _Badge(
+              'assets/worker-svgrepo-com.svg',
+              size: widgetSize,
+              borderColor: const Color(0xff13d38e),
+            ),
+            badgePositionPercentageOffset: .98,
           );
         default:
           return null;
       }
     });
+  }
+}
+
+class _Badge extends StatelessWidget {
+  final String svgAsset;
+  final double size;
+  final Color borderColor;
+
+  const _Badge(
+    this.svgAsset, {
+    Key key,
+    @required this.size,
+    @required this.borderColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: PieChart.defaultDuration,
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: borderColor,
+          width: 2,
+        ),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black.withOpacity(.5),
+            offset: const Offset(3, 3),
+            blurRadius: 3,
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(size * .15),
+      child: Center(
+        child: SvgPicture.asset(
+          svgAsset,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
   }
 }
