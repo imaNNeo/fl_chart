9d24d098c8604b3873df2cf76b1f12c8cf4b5531
imaNNeoFighT <iman.neofight@gmail.com>
2021-03-18 00:22:08 +0330

    Use functional approach for drawing in painter classes (We don't keep chart's data anymore, instead we pass it through draw functions).

--- lib/src/chart/base/base_chart/chart_painter.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-/// Abstract class for our painters
-///
-/// This class contains [paint] method, that takes a [canvas], and [size] for drawing the content.
-abstract class ChartPainter {
-  void paint(Canvas canvas, Size size);
-}
