c50cc5f6e8f0d878e326e45d76e60f0191578dc4
jonas.hoefer <jonas.hoefer@iwelt.de>
2021-04-20 14:38:36 +0200

    Fix index out of bounds when changing to another line chart data of different length while indicator is set too high for new data

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -291,7 +291,10 @@ class LineChartPainter extends AxisChartPainter<LineChartData> {
 
     for (var i = 0; i < barData.showingIndicators.length; i++) {
       final indicatorData = indicatorsData[i];
-      final index = barData.showingIndicators[i];
+      var index = barData.showingIndicators[i];
+      if (index >= barData.spots.length) {
+        index = barData.spots.length - 1;
+      }
       final spot = barData.spots[index];
 
       if (indicatorData == null) {
