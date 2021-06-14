168669784b78c72ccbb6f5f3feb7dfdf2c021b11
Iman Khoshabi <iman.neofight@gmail.com>
2021-06-14 23:05:12 +0430

    Initial commit fl_chart_app

---
+++ fl_chart_app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('FL Chart'),
+        ),
+      ),
+    );
+  }
+}
