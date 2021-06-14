aee7dfa5204966e5add76e654530cefd2b7f9d52
Iman Khoshabi <iman.neofight@gmail.com>
2021-06-15 00:27:48 +0430

    Implement menu_row.dart

---
+++ fl_chart_app/lib/resources/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static final icLineChart = 'assets/icons/ic_line_chart.svg';
+  static final icBarChart = 'assets/icons/ic_bar_chart.svg';
+  static final icPieChart = 'assets/icons/ic_pie_chart.svg';
+  static final icScatterChart = 'assets/icons/ic_scatter_chart.svg';
+  static final icRadarChart = 'assets/icons/ic_radar_chart.svg';
+}
