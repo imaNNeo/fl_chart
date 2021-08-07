ab5277e9b6ca344694ade90477f42be2efcd6e8b
Iman Khoshabi <iman.neofight@gmail.com>
2021-08-07 12:09:00 +0430

    Use drawer instead of separated page for menu in small width devices.

--- fl_chart_app/lib/resources/app_dimens.dart
+++ fl_chart_app/lib/resources/app_dimens.dart
@@ -1,5 +1,5 @@
 class AppDimens {
-  static const double menuMaxNeededWidth = 400;
+  static const double menuMaxNeededWidth = 304;
   static const double menuRowHeight = 56;
   static const double menuRowVerticalSpace = 18.5;
   static const double menuIconSize = 36;
