ae4d07e2ac307a37cb07eb0cc2553de50bda2ed0
Iman Khoshabi <iman.neofight@gmail.com>
2021-08-19 00:12:56 +0430

    Create abstract FlTouchEvent and some concrete events to migrate to our new touch handling system. We will be able to support all kind of touch/pointer events such as longPress, onTapUp, onPanMove. #649.

--- lib/fl_chart.dart
+++ lib/fl_chart.dart
@@ -4,6 +4,7 @@ export 'src/chart/bar_chart/bar_chart.dart';
 export 'src/chart/bar_chart/bar_chart_data.dart';
 export 'src/chart/base/axis_chart/axis_chart_data.dart';
 export 'src/chart/base/base_chart/base_chart_data.dart';
+export 'src/chart/base/base_chart/fl_touch_event.dart';
 export 'src/chart/line_chart/line_chart.dart';
 export 'src/chart/line_chart/line_chart_data.dart';
 export 'src/chart/pie_chart/pie_chart.dart';
