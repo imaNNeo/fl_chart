c08f8a2aa9eb8880db07841458ea9d7753ebfbfb
bakatsuyuki <bakatsuyuki@icloud.com>
2021-08-23 21:34:22 +0900

    Refactor BarChartSample3.

--- example/lib/bar_chart/samples/bar_chart_sample3.dart
+++ example/lib/bar_chart/samples/bar_chart_sample3.dart
@@ -1,6 +1,130 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
+class _BarChart extends StatelessWidget {
+  const _BarChart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barTouchData: barTouchData,
+        titlesData: titlesData,
+        borderData: borderData,
+        barGroups: barGroups,
+        alignment: BarChartAlignment.spaceAround,
+        maxY: 20,
+      ),
+    );
+  }
+
+  BarTouchData get barTouchData => BarTouchData(
+        enabled: false,
+        touchTooltipData: BarTouchTooltipData(
+          tooltipBgColor: Colors.transparent,
+          tooltipPadding: const EdgeInsets.all(0),
+          tooltipMargin: 8,
+          getTooltipItem: (
+            BarChartGroupData group,
+            int groupIndex,
+            BarChartRodData rod,
+            int rodIndex,
+          ) {
+            return BarTooltipItem(
+              rod.y.round().toString(),
+              TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
+      );
+
+  FlTitlesData get titlesData => FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => const TextStyle(
+            color: Color(0xff7589a2),
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+          margin: 20,
+          getTitles: (double value) {
+            switch (value.toInt()) {
+              case 0:
+                return 'Mn';
+              case 1:
+                return 'Te';
+              case 2:
+                return 'Wd';
+              case 3:
+                return 'Tu';
+              case 4:
+                return 'Fr';
+              case 5:
+                return 'St';
+              case 6:
+                return 'Sn';
+              default:
+                return '';
+            }
+          },
+        ),
+        leftTitles: SideTitles(showTitles: false),
+      );
+
+  FlBorderData get borderData => FlBorderData(
+        show: false,
+      );
+
+  List<BarChartGroupData> get barGroups => [
+        BarChartGroupData(
+          x: 0,
+          barRods: [
+            BarChartRodData(y: 8, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 1,
+          barRods: [
+            BarChartRodData(y: 10, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 2,
+          barRods: [
+            BarChartRodData(y: 14, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 3,
+          barRods: [
+            BarChartRodData(y: 15, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 3,
+          barRods: [
+            BarChartRodData(y: 13, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 3,
+          barRods: [
+            BarChartRodData(y: 10, colors: [Colors.lightBlueAccent, Colors.greenAccent])
+          ],
+          showingTooltipIndicators: [0],
+        ),
+      ];
+}
+
 class BarChartSample3 extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => BarChartSample3State();
@@ -15,111 +139,7 @@ class BarChartSample3State extends State<BarChartSample3> {
         elevation: 0,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
         color: const Color(0xff2c4260),
-        child: BarChart(
-          BarChartData(
-            alignment: BarChartAlignment.spaceAround,
-            maxY: 20,
-            barTouchData: BarTouchData(
-              enabled: false,
-              touchTooltipData: BarTouchTooltipData(
-                tooltipBgColor: Colors.transparent,
-                tooltipPadding: const EdgeInsets.all(0),
-                tooltipMargin: 8,
-                getTooltipItem: (
-                  BarChartGroupData group,
-                  int groupIndex,
-                  BarChartRodData rod,
-                  int rodIndex,
-                ) {
-                  return BarTooltipItem(
-                    rod.y.round().toString(),
-                    TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                },
-              ),
-            ),
-            titlesData: FlTitlesData(
-              show: true,
-              bottomTitles: SideTitles(
-                showTitles: true,
-                getTextStyles: (context, value) => const TextStyle(
-                    color: Color(0xff7589a2), fontWeight: FontWeight.bold, fontSize: 14),
-                margin: 20,
-                getTitles: (double value) {
-                  switch (value.toInt()) {
-                    case 0:
-                      return 'Mn';
-                    case 1:
-                      return 'Te';
-                    case 2:
-                      return 'Wd';
-                    case 3:
-                      return 'Tu';
-                    case 4:
-                      return 'Fr';
-                    case 5:
-                      return 'St';
-                    case 6:
-                      return 'Sn';
-                    default:
-                      return '';
-                  }
-                },
-              ),
-              leftTitles: SideTitles(showTitles: false),
-            ),
-            borderData: FlBorderData(
-              show: false,
-            ),
-            barGroups: [
-              BarChartGroupData(
-                x: 0,
-                barRods: [
-                  BarChartRodData(y: 8, colors: [Colors.lightBlueAccent, Colors.greenAccent])
-                ],
-                showingTooltipIndicators: [0],
-              ),
-              BarChartGroupData(
-                x: 1,
-                barRods: [
-                  BarChartRodData(y: 10, colors: [Colors.lightBlueAccent, Colors.greenAccent])
-                ],
-                showingTooltipIndicators: [0],
-              ),
-              BarChartGroupData(
-                x: 2,
-                barRods: [
-                  BarChartRodData(y: 14, colors: [Colors.lightBlueAccent, Colors.greenAccent])
-                ],
-                showingTooltipIndicators: [0],
-              ),
-              BarChartGroupData(
-                x: 3,
-                barRods: [
-                  BarChartRodData(y: 15, colors: [Colors.lightBlueAccent, Colors.greenAccent])
-                ],
-                showingTooltipIndicators: [0],
-              ),
-              BarChartGroupData(
-                x: 3,
-                barRods: [
-                  BarChartRodData(y: 13, colors: [Colors.lightBlueAccent, Colors.greenAccent])
-                ],
-                showingTooltipIndicators: [0],
-              ),
-              BarChartGroupData(
-                x: 3,
-                barRods: [
-                  BarChartRodData(y: 10, colors: [Colors.lightBlueAccent, Colors.greenAccent])
-                ],
-                showingTooltipIndicators: [0],
-              ),
-            ],
-          ),
-        ),
+        child: const _BarChart(),
       ),
     );
   }
