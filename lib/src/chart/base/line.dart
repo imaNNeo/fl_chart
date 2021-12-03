0d101db5489e6c307a748390f1dd18b9bcfcbc81
Iman Khoshabi <iman.neofight@gmail.com>
2021-12-03 20:26:13 +0330

    Add `magnitude()`, `direction()`, `normalize()` functions in line.dart

--- lib/src/chart/base/line.dart
+++ lib/src/chart/base/line.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/painting.dart';
+import 'dart:math' as math;
 
 /// Describes a line model (contains [from], and end [to])
 class Line {
@@ -9,4 +10,24 @@ class Line {
   final Offset to;
 
   Line(this.from, this.to);
+
+  /// Returns the length of line
+  double magnitude() {
+    final diff = to - from;
+    final dx = diff.dx;
+    final dy = diff.dy;
+    return math.sqrt(dx * dx + dy * dy);
+  }
+
+  /// Returns angle of the line in radians
+  double direction() {
+    final diff = to - from;
+    return math.atan(diff.dy / diff.dx);
+  }
+
+  /// Returns the line in magnitude of 1.0
+  Offset normalize() {
+    final diffOffset = to - from;
+    return diffOffset * (1.0 / magnitude());
+  }
 }
