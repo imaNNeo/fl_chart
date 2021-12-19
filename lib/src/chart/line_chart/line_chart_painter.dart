5a08641c4cbf15e484da5e43b55d16c57a46046a
Iman Khoshabi <iman.neofight@gmail.com>
2021-12-19 19:28:19 +0330

    Remove canvas_extension and move drawDashedLine() to canvas_wrapper class

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -4,7 +4,6 @@ import 'dart:ui' as ui;
 import 'package:fl_chart/fl_chart.dart';
 import 'package:fl_chart/src/chart/base/axis_chart/axis_chart_painter.dart';
 import 'package:fl_chart/src/chart/base/base_chart/base_chart_painter.dart';
-import 'package:fl_chart/src/extensions/canvas_extension.dart';
 import 'package:fl_chart/src/extensions/paint_extension.dart';
 import 'package:fl_chart/src/extensions/path_extension.dart';
 import 'package:fl_chart/src/utils/canvas_wrapper.dart';
