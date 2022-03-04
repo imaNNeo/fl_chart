55fac34c41ba7e1fe28709f3f927d7200d529855
Iman Khoshabi <iman.neofight@gmail.com>
2022-03-05 02:06:44 +0330

    Add bar_chart_sample6 to represent group vertical bars

---
+++ example/lib/legend_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class LegendWidget extends StatelessWidget {
+  final String name;
+  final Color color;
+
+  const LegendWidget({
+    Key? key,
+    required this.name,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(width: 6),
+        Text(
+          name,
+          style: const TextStyle(
+            color: Color(0xff757391),
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LegendsListWidget extends StatelessWidget {
+  final List<Legend> legends;
+
+  const LegendsListWidget({
+    Key? key,
+    required this.legends,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 16,
+      children: legends
+          .map(
+            (e) => LegendWidget(
+              name: e.name,
+              color: e.color,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
+
+class Legend {
+  final String name;
+  final Color color;
+
+  Legend(this.name, this.color);
+}
