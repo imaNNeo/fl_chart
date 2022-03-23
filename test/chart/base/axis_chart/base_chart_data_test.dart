d685e95ca2824aa5d357507fa06c35543e1335d0
Iman Khoshabi <iman.neofight@gmail.com>
2022-03-23 17:36:54 +0430

    Move axis_chart*_test to axis_chart/ directory

--- test/chart/base/axis_chart/base_chart_data_test.dart
+++ test/chart/base/axis_chart/base_chart_data_test.dart
@@ -1,7 +1,7 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../data_pool.dart';
+import '../../data_pool.dart';
 
 void main() {
   group('BaseChartData data equality check', () {
