1ae6a6d37ccbf4b595ed40adaa818cecaf49801b
Iman Khoshabi <iman.neofight@gmail.com>
2022-05-19 00:04:25 +0430

    Fix pie_chart formatting issue

--- lib/src/chart/pie_chart/pie_chart.dart
+++ lib/src/chart/pie_chart/pie_chart.dart
@@ -50,7 +50,7 @@ class _PieChartState extends AnimatedWidgetBaseState<PieChart> {
 
   /// This allows a value of type T or T? to be treated as a value of type T?.
   ///
-  /// We use this so that APIs that have become non-nullable can still be used 
+  /// We use this so that APIs that have become non-nullable can still be used
   /// with `!` and `?` to support older versions of the API as well.
   T? _ambiguate<T>(T? value) => value;
 
