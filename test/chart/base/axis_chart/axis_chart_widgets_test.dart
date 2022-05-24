2bed282cf5f0a98813044674e4b10cd33419aa0a
Iman Khoshabi <iman.neofight@gmail.com>
2022-05-24 23:48:48 +0430

    Remove deprecated annotation in canvas_wrapper, there is no problem with using it as a holder.

--- test/chart/base/axis_chart/axis_chart_widgets_test.dart
+++ test/chart/base/axis_chart/axis_chart_widgets_test.dart
@@ -4,16 +4,68 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets(
-    'SideTitleWidget',
+    'SideTitleWidget left',
     (WidgetTester tester) async {
       await tester.pumpWidget(
         const MaterialApp(
           home: Scaffold(
-            body: Center(
-              child: SideTitleWidget(
-                axisSide: AxisSide.left,
-                child: Text("1s"),
-              ),
+            body: SideTitleWidget(
+              axisSide: AxisSide.left,
+              child: Text("1s"),
+            ),
+          ),
+        ),
+      );
+      expect(find.byType(Container), findsOneWidget);
+      expect(find.text('1s'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'SideTitleWidget top',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: SideTitleWidget(
+              axisSide: AxisSide.top,
+              child: Text("1s"),
+            ),
+          ),
+        ),
+      );
+      expect(find.byType(Container), findsOneWidget);
+      expect(find.text('1s'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'SideTitleWidget right',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: SideTitleWidget(
+              axisSide: AxisSide.right,
+              child: Text("1s"),
+            ),
+          ),
+        ),
+      );
+      expect(find.byType(Container), findsOneWidget);
+      expect(find.text('1s'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'SideTitleWidget bottom',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: SideTitleWidget(
+              axisSide: AxisSide.bottom,
+              child: Text("1s"),
             ),
           ),
         ),
