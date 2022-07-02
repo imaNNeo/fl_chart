e347809b566c6a0c14479595cb4b754876bf1f83
Robin Jespersen <jespersen@unitedpartners.de>
2022-07-02 22:33:08 +0200

    updated extension

--- lib/src/extensions/side_titles_extension.dart
+++ lib/src/extensions/side_titles_extension.dart
@@ -1,13 +1,24 @@
 import 'package:fl_chart/src/chart/base/axis_chart/axis_chart_data.dart';
 
 extension SideTitlesExtension on AxisTitles {
-  double get totalReservedSize {
+  double get totalReservedWidth {
     var size = 0.0;
     if (showAxisTitles) {
       size += axisNameSize;
     }
     if (showSideTitles) {
-      size += sideTitles.reservedSize;
+      size += sideTitles.reservedWidth ?? 11;
+    }
+    return size;
+  }
+
+  double get totalReservedHeight {
+    var size = 0.0;
+    if (showAxisTitles) {
+      size += axisNameSize;
+    }
+    if (showSideTitles) {
+      size += sideTitles.reservedHeight ?? 11;
     }
     return size;
   }
