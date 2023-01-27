d8f05a830566eecd13276c0ea9231e1da1fdf65c
imaNNeoFighT <iman.neofight@gmail.com>
2023-01-27 21:31:17 +0330

    Update all barChart sample colors

--- example_new/lib/util/extensions/color_extensions.dart
+++ example_new/lib/util/extensions/color_extensions.dart
@@ -12,4 +12,12 @@ extension ColorExtension on Color {
       (blue * value).round(),
     );
   }
+
+  Color avg(Color other) {
+    final red = (this.red + other.red) ~/ 2;
+    final green = (this.green + other.green) ~/ 2;
+    final blue = (this.blue + other.blue) ~/ 2;
+    final alpha = (this.alpha + other.alpha) ~/ 2;
+    return Color.fromARGB(alpha, red, green, blue);
+  }
 }
