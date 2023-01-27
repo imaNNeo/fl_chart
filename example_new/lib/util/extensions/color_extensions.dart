599a4650a59a108de3357217f8ea98a2a71cb869
imaNNeoFighT <iman.neofight@gmail.com>
2023-01-27 22:46:35 +0330

    Update all pieChart sample colors

--- example_new/lib/util/extensions/color_extensions.dart
+++ example_new/lib/util/extensions/color_extensions.dart
@@ -13,6 +13,17 @@ extension ColorExtension on Color {
     );
   }
 
+  Color lighten([int percent = 40]) {
+    assert(1 <= percent && percent <= 100);
+    final value = percent / 100;
+    return Color.fromARGB(
+      alpha,
+      (red + ((255 - red) * value)).round(),
+      (green + ((255 - green) * value)).round(),
+      (blue + ((255 - blue) * value)).round(),
+    );
+  }
+
   Color avg(Color other) {
     final red = (this.red + other.red) ~/ 2;
     final green = (this.green + other.green) ~/ 2;
