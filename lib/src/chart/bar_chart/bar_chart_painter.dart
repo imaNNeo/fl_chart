5ee106a8e08b1d808f97002594b4b5dcbb35f847
apeksha mehta <>
2023-08-29 14:08:37 +0530

    solved merge conflicts

--- lib/src/chart/bar_chart/bar_chart_painter.dart
+++ lib/src/chart/bar_chart/bar_chart_painter.dart
@@ -463,9 +463,6 @@ class BarChartPainter extends AxisChartPainter<BarChartData> {
       bottomLeft: radius,
       bottomRight: radius,
     );
-    // _bgTouchTooltipPaint.color = tooltipData.tooltipBgColor;
-
-    /// set tooltip's background color for each rod
     _bgTouchTooltipPaint.color = tooltipData.getTooltipColor(showOnBarGroup);
 
     final rotateAngle = tooltipData.rotateAngle;
