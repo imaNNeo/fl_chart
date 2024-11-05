40479f04fd080a5c13ed5882e1ded3e0cd33766b
sosna <jakub.sosnowski2001@gmail.com>
2024-11-05 18:12:42 +0100

    Add shadow params to LineTouchTooltipData class

--- lib/src/chart/line_chart/line_chart_data.dart
+++ lib/src/chart/line_chart/line_chart_data.dart
@@ -1059,6 +1059,9 @@ class LineTouchTooltipData with EquatableMixin {
     this.showOnTopOfTheChartBoxArea = false,
     this.rotateAngle = 0.0,
     this.tooltipBorder = BorderSide.none,
+    this.shadowColor = Colors.transparent,
+    this.shadowBlur = 5,
+    this.shadowOffset = Offset.zero,
   }) : _tooltipBorderRadius = tooltipBorderRadius;
 
   /// Sets a rounded radius for the tooltip.
@@ -1104,6 +1107,15 @@ class LineTouchTooltipData with EquatableMixin {
   // /// Retrieves data for setting background color of the tooltip.
   final GetLineTooltipColor getTooltipColor;
 
+  /// The shadow color of the tooltip.
+  final Color shadowColor;
+
+  /// The sigma value of shadow blur of the tooltip.
+  final double shadowBlur;
+
+  /// The shadow offset of the tooltip.
+  final Offset shadowOffset;
+
   /// Used for equality check, see [EquatableMixin].
   @override
   List<Object?> get props => [
@@ -1120,6 +1132,9 @@ class LineTouchTooltipData with EquatableMixin {
         rotateAngle,
         tooltipBorder,
         getTooltipColor,
+        shadowColor,
+        shadowBlur,
+        shadowOffset,
       ];
 }
 
