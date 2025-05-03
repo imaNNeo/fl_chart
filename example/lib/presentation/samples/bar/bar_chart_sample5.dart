91b4f4e048d42e2e71ffa96b54aaf6e69ac71ef5
alikhavarii13 <sdyarash@gmail.com>
2025-05-03 13:00:11 +0330

    fixed problems

--- example/lib/presentation/samples/bar/bar_chart_sample5.dart
+++ example/lib/presentation/samples/bar/bar_chart_sample5.dart
@@ -167,7 +167,6 @@ class BarChartSample5State extends State<BarChartSample5> {
                 ),
           rodStackItems: [
             BarChartRodStackItem(
-
               0,
               value1,
               AppColors.contentColorGreen,
