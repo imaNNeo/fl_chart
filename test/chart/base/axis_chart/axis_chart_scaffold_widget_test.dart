5039cd9e489649047b746a865ffbb17fbea280f5
Amin Memariani <amin90memar@gmail.com>
2025-12-21 14:21:04 +0300

    Fix remaining find.byType(ColoredBox) reference

--- test/chart/base/axis_chart/axis_chart_scaffold_widget_test.dart
+++ test/chart/base/axis_chart/axis_chart_scaffold_widget_test.dart
@@ -1072,7 +1072,7 @@ void main() {
 
           // Verify that scaling actually occurred
           final renderBox = tester.renderObject<RenderBox>(
-            find.byType(ColoredBox),
+            find.byKey(chartColoredBoxKey),
           );
 
           // Wait until chartVirtualRect is updated and scaling is complete
