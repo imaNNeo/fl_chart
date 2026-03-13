3219da433ca8030445116a656e4344b04361d941
imaNNeoFighT <iman.neofight@gmail.com>
2026-03-13 17:56:31 +0100

    Render the bar labels in the bar_chart_painter

--- lib/src/chart/bar_chart/bar_chart_painter.dart
+++ lib/src/chart/bar_chart/bar_chart_painter.dart
@@ -93,6 +93,7 @@ class BarChartPainter extends AxisChartPainter<BarChartData> {
     }
 
     drawBars(canvasWrapper, _groupBarsPosition!, holder);
+    drawBarLabels(context, canvasWrapper, _groupBarsPosition!, holder);
 
     drawErrorIndicatorData(canvasWrapper, _groupBarsPosition!, holder);
 
@@ -411,6 +412,61 @@ class BarChartPainter extends AxisChartPainter<BarChartData> {
             );
           }
         }
+      }
+    }
+  }
+
+  @visibleForTesting
+  void drawBarLabels(
+    BuildContext context,
+    CanvasWrapper canvasWrapper,
+    List<GroupBarsPosition> groupBarsPosition,
+    PaintHolder<BarChartData> holder,
+  ) {
+    final data = holder.data;
+    final viewSize = canvasWrapper.size;
+
+    for (var i = 0; i < data.barGroups.length; i++) {
+      final barGroup = data.barGroups[i];
+      for (var j = 0; j < barGroup.barRods.length; j++) {
+        final barRod = barGroup.barRods[j];
+        final rodLabel = barRod.label;
+        if (rodLabel == null || !rodLabel.show) continue;
+
+        final labelText = rodLabel.text;
+        final labelStyle = rodLabel.style;
+        if (labelText.isEmpty) continue;
+
+        final effectiveStyle =
+            Utils().getThemeAwareTextStyle(context, labelStyle);
+
+        final textSpan = TextSpan(text: labelText, style: effectiveStyle);
+        final textPainter = TextPainter(
+          text: textSpan,
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.ltr,
+          textScaler: holder.textScaler,
+        )..layout();
+
+        final x = groupBarsPosition[i].barsX[j];
+        final tipPixelY = getPixelY(barRod.toY, viewSize, holder);
+        final isUpward = barRod.toY >= barRod.fromY;
+
+        final drawX = x + rodLabel.offset.dx;
+        final drawY = isUpward
+            ? tipPixelY - rodLabel.offset.dy - textPainter.height
+            : tipPixelY + rodLabel.offset.dy;
+
+        final chartRotation = -data.rotationQuarterTurns * (pi / 2);
+        final labelAngle = rodLabel.angle * (pi / 180);
+
+        canvasWrapper
+          ..save()
+          ..translate(drawX, drawY + textPainter.height / 2)
+          ..rotate(chartRotation + labelAngle)
+          ..translate(-textPainter.width / 2, -textPainter.height / 2);
+        textPainter.paint(canvasWrapper.canvas, Offset.zero);
+        canvasWrapper.restore();
       }
     }
   }
